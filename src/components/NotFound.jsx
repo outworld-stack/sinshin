b944ae9f0a885eca8f4293179b5208859c244bff
o s <hija1@chmail.ir>
2025-07-30 01:28:00 +0330

    solve a issue

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router'
 export default function NotFound() {
   return (
     <section class="bg-white dark:bg-gray-900">
-      <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+      <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-18 lg:px-6">
         <div class="mx-auto max-w-screen-sm text-center">
           <h1 class="mb-4 text-7xl tracking-tight font-DanaDemiBold lg:text-9xl text-primary-600 dark:text-primary-500">404</h1>
           <p class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">مثل این که اشتباه اومدی -__-</p>
